b5cb9c5b3e7c1ca72581fb8698b83e1b241265c2
Josh Billingham <wjbillin@umich.edu>
2013-11-05 19:54:42 -0500

    download multiple ranges

--- download_data.py
+++ download_data.py
@@ -13,10 +13,8 @@ import os
 import sys
 
 ITEMS_PER_API_CALL = 50
-FILE_PATTERN = re.compile('\w*_\d_\d.pickle')
 
-def download(examples_to_download, save_dir):
-  save_file = os.path.expanduser(save_dir)+"raw_data.pickle"
+def download(examples_to_download, offset, save_file):
 
   nix = Nutritionix(app_id=private_consts.NUTRITIONIX["app_id"],
     api_key=private_consts.NUTRITIONIX["api_key"])
@@ -24,7 +22,7 @@ def download(examples_to_download, save_dir):
   items = []
   num_calls = examples_to_download/ITEMS_PER_API_CALL
   for i in range(num_calls):
-    print "Downloading items {0} through {1}".format(i*ITEMS_PER_API_CALL, (i+1)*ITEMS_PER_API_CALL)
+    print "Downloading items {0} through {1}".format(i*ITEMS_PER_API_CALL + offset, (i+1)*ITEMS_PER_API_CALL + offset)
     results = nix.search().nxql(
       filters = {
         "item_type":1,
@@ -35,7 +33,7 @@ def download(examples_to_download, save_dir):
           "gt":0
         }
       },
-      offset = i*ITEMS_PER_API_CALL,
+      offset = i*ITEMS_PER_API_CALL + offset,
       limit = ITEMS_PER_API_CALL
     ).json()
 
@@ -44,23 +42,26 @@ def download(examples_to_download, save_dir):
   # Save data
   pickle.dump( items, open( save_file, "wb" ) )
 
+  return
+
+def create_new_file(num, offset):
+  end = int(num) + int(offset);
+  return os.path.expanduser(private_consts.SAVE_DIR) + "raw_data_" + offset + "_" + str(end) + ".pickle"
+
+# Beginning of execution.
 if __name__ == "__main__":
-  usage_error = "\nUsage: python download_data.py 1000 # Download 1000 " +
-          "examples, or\n" +
-          "python download_data.py raw_data_0_100.pickle 1000 # Download 100 " +
-          "examples and append them to raw_data_0_100.pickle, forming a new file" +
-          " raw_data_0_200.pickle"
+  usage_error = "\nUsage: python download_data.py 1000 # Download 1000 examples, or\n python download_data.py 1000 200 # Download 1000 examples, with an offset of 200"
   
-  if len(sys.argv) < 2:
+  if len(sys.argv) < 2 or len(sys.argv) > 3:
     print usage_error
     exit(1)
 
   num_to_download = 0
-  if (FILE_PATTERN.match(sys.argv[1])):
-    if len(sys.argv) != 3:
-      print usage_error
-      exit(1)
-    num_to_download = int(sys.argv[2])
+  offset = 0
+  if len(sys.argv) == 3:
+    save_file = create_new_file(sys.argv[1], sys.argv[2])
+    download(int(sys.argv[1]), int(sys.argv[2]), save_file)
+
   else:
-    num_to_download = int(sys.argv[1])
-    download(num_to_download, private_consts.SAVE_DIR)
+    save_file = create_new_file(sys.argv[1], "0")
+    download(int(sys.argv[1]), 0, save_file)
