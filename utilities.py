2002f106c1740ff49b6cd278f1daff70299d7dae
justkunz <Justine@67-194-46-207.wireless.umnet.umich.edu>
2013-11-07 17:49:00 -0500

    adding a printing function

---
+++ utilities.py
@@ -0,0 +1,9 @@
+
+
+def pretty_print_predictions(x_test, t_test, t_out, data_size):
+  (x, t, v) = load_data(data_size)
+
+  for i in range(len(x_test))
+    print [vocabulary[j] in range(len(x_test[i])) if (x_test[i][j] == 1)]
+    
+  
